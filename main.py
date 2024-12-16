49a04b73f11de7151561b4e5ef14f992b17381c0
Jonathan <alexisalexis.wy@gmail.com>
2024-12-16 21:12:28 +0630

    first test product details

--- main.py
+++ main.py
@@ -18,5 +18,20 @@ soup = BeautifulSoup(response.text, "html.parser")
 script_tag = soup.find("script", id="__NEXT_DATA__")
 
 data = json.loads(script_tag.string)
+initial_data = data["props"]["pageProps"]["initialData"]["data"]
+product_data = initial_data["product"]
+reviews_data = initial_data.get("reviews", {})
 
-print(data['props']['pageProps']['initialData']['data']['product']['priceInfo']['currentPrice']['price'])
+product_info = {
+                "price": product_data["priceInfo"]["currentPrice"]["price"],
+                "review_count": reviews_data.get("totalReviewCount", 0),
+                "item_id": product_data["usItemId"],
+                "avg_rating": reviews_data.get("averageOverallRating", 0),
+                "product_name": product_data["name"],
+                "brand": product_data.get("brand", ""),
+                "availability": product_data["availabilityStatus"],
+                "image_url": product_data["imageInfo"]["thumbnailUrl"],
+                "short_description": product_data.get("shortDescription", "")
+            }
+
+print(product_info)
