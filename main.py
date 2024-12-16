a3949df122cbd030c0fb35737fdb82132cae512d
Jonathan <alexisalexis.wy@gmail.com>
2024-12-16 21:10:01 +0630

    tracking " data.key() " one by one. First one is " ['props'] , etc..."

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
 walmart_url = "https://www.walmart.com/ip/onn-34-Curved-Ultrawide-WQHD-3440-x-1440p-100Hz-Bezel-Less-Office-Monitor-with-Cable-Black/2522348721"
 
-response = requests.get(walmart_url)
+HEADERS = {
+    "Accept" : "*/*",
+    "Accept-Encoding" : "gzip, deflate, br, zstd",
+    "Accept-Language" : "en-US,en;q=0.9",
+    "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+}
 
-html = response.text
+response = requests.get(walmart_url, headers=HEADERS)
 
-print(html)
+soup = BeautifulSoup(response.text, "html.parser")
+
+script_tag = soup.find("script", id="__NEXT_DATA__")
+
+data = json.loads(script_tag.string)
+
+print(data['props']['pageProps']['initialData']['data']['product']['priceInfo']['currentPrice']['price'])
