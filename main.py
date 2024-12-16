238f3150ccc6b8f76452dc67c79fe68ca38a8f3b
Jonathan <alexisalexis.wy@gmail.com>
2024-12-16 21:53:21 +0630

    done scrapping product data while looping the page within related products and then store it as "json" in the host device. Only problem is that having duplicated (including same product in the json)

--- main.py
+++ main.py
@@ -11,18 +11,44 @@ HEADERS = {
     "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
 }
 
-response = requests.get(walmart_url, headers=HEADERS)
+def get_product_links(query, page_number=1):
+  search_url = f"https://www.walmart.com/search?q={query}&page={page_number}"  #use "f string" to flip out search term and take whatever query
 
-soup = BeautifulSoup(response.text, "html.parser")
+  response = requests.get(search_url, headers=HEADERS)
 
-script_tag = soup.find("script", id="__NEXT_DATA__")
+  soup = BeautifulSoup(response.text, "html.parser")
 
-data = json.loads(script_tag.string)
-initial_data = data["props"]["pageProps"]["initialData"]["data"]
-product_data = initial_data["product"]
-reviews_data = initial_data.get("reviews", {})
+  links = soup.find_all('a', href=True)   #use this to find related links which might be sponsors
 
-product_info = {
+  product_links = []
+
+  for link in links:
+    link_href = link['href']
+    if "/ip/" in link_href:
+      if "https" in link_href:
+        full_url = link_href
+      else: 
+        full_url = "https://www.walmart.com" + link_href
+      
+      product_links.append(full_url)
+
+  return product_links
+
+
+
+def extract_product_info(product_url):
+  response = requests.get(product_url, headers=HEADERS)
+
+  soup = BeautifulSoup(response.text, "html.parser")
+
+  script_tag = soup.find("script", id="__NEXT_DATA__")
+
+  data = json.loads(script_tag.string)
+  initial_data = data["props"]["pageProps"]["initialData"]["data"]
+  product_data = initial_data["product"]
+  reviews_data = initial_data.get("reviews", {})
+
+  product_info = {
                 "price": product_data["priceInfo"]["currentPrice"]["price"],
                 "review_count": reviews_data.get("totalReviewCount", 0),
                 "item_id": product_data["usItemId"],
@@ -32,6 +58,33 @@ product_info = {
                 "availability": product_data["availabilityStatus"],
                 "image_url": product_data["imageInfo"]["thumbnailUrl"],
                 "short_description": product_data.get("shortDescription", "")
-            }
+    }
 
-print(product_info)
+  return product_info
+
+
+def main():
+  OUTPUT_FILE = "product_info.jsonLink"
+
+  with open(OUTPUT_FILE, 'w') as file: 
+    page_number = 1
+    while True:
+      links = get_product_links("computer", page_number)
+      if not links or page_number > 99:
+        break
+
+      for link in links:
+        try:
+            product_info = extract_product_info(link)
+            if product_info:
+              file.write(json.dumps(product_info) + "\n" )
+        
+        except Exception as e:
+          print(f"Failed to process URL {link}. Error {e}")
+        
+      page_number += 1 
+      print(f"Search page {page_number}") 
+
+
+if __name__ == "__main__":
+  main()
